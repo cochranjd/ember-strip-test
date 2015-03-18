86f39a2d5f5bf12d2df0589ec8010036bbd310c4
Joshua Cochran <josh.cochran@gmail.com>
2015-03-17 21:45:03 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var removeFile = require('broccoli-file-remover');
+
+module.exports = {
+  name: 'ember-build-flags',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    app.registry.add('js', {
+      name: 'ember-build-flags',
+      ext: 'js',
+      toTree: function(tree) {
+        var treeA = removeFile(tree, {
+          srcFile: 'client/initializers/test-server.js',
+        });
+        var treeB = removeFile(treeA, {
+          paths: ['client/fixtures', 'client/tests']
+        });
+
+        return treeB;
+      }
+    });
+  }
+};
